79f419aa3159076f8087c032637e86681eb74d9f
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-07-22 19:33:19 +0200

    improved time complexity from n^2 to nlog(n)

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -14,3 +14,43 @@ public:
         return vector<int>();
     }
 };
+
+// ===========================================
+
+struct Element
+{
+    int index, value;
+    Element(int a, int b) : index(a), value(b) {}
+    Element() = default;
+    bool operator<(const Element &other) const
+    {
+        return value != other.value ? (value < other.value) : (index < other.index);
+    }
+    bool operator<(int other) const
+    {
+        return value < other;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        // sort nums without loosing information about indexes
+        vector<Element> temp;
+        for (int i = 0; i < nums.size(); ++i)
+            temp.push_back(Element(i, nums[i]));
+        sort(temp.begin(), temp.end());
+
+        // for every element of nums find with binary search if element exists
+        for (auto it = temp.begin(); it != temp.end(); ++it)
+        {
+            int seekedValue = target - (*it).value;
+            auto foundElement = lower_bound(it + 1, temp.end(), seekedValue);
+            if (foundElement != temp.end() && (*foundElement).value + (*it).value == target)
+                return vector<int>{(*foundElement).index, (*it).index};
+        }
+        return vector<int>();
+    }
+};
